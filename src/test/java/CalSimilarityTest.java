4b490b93c2d800ae56a3e28b9c412538f267c341
swcheng <cheng_deeplove@163.com>
2019-04-09 21:37:21 +0800

    完成部分minHash的编写

--- src/test/java/CalSimilarityTest.java
+++ src/test/java/CalSimilarityTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 import java.io.FileReader;
 import java.io.Reader;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
@@ -42,14 +43,44 @@ public class CalSimilarityTest {
             }
             System.out.println("当前第" + (++fileNumCount) + "个文件");
         }
-        // 不可以直接创建这样的矩阵，因为内存不允许
-        int[][] usernewsMatrix = new int[User.userConstructSerialNumber][News.itemSerialNumber];
+        // 不可以直接创建这样的矩阵，因为内存不允许，所以我们需要构建hash函数去缩小这个
+        // 矩阵的大小，问题是如何构建五十个hash函数
+        int[][] minHash = new int[User.userConstructSerialNumber][50];
+        for (int j = 0; j < User.userConstructSerialNumber; j++) {
+            Arrays.fill(minHash[j], Integer.MAX_VALUE);
+        }
         for (Map.Entry<String, User> entry: User.userIdORM.entrySet()){
             User user = entry.getValue();
-            for (News item :user.getReadList())
-                usernewsMatrix[user.getMatrixNumber()][item.getMatrixNumber()] = 1;
+            for (int l = 0; l < 50; l++) {
+                // 对于每个用户，其中的五十个hash函数我们仅仅需要对每个hash函数去计算它们存在有新闻的行号，
+                // 这样减少了计算量
+                for (News item :user.getReadList()){
+                    if (minHash[user.getMatrixNumber()][l] > handleHashFunction(l, item.getMatrixNumber()))
+                        minHash[user.getMatrixNumber()][l] =  handleHashFunction(l, item.getMatrixNumber());
+                }
+            }
         }
-        System.out.println("用户新闻矩阵初步构建完成");
+
+    }
+
+    /**
+     * 返回minHash签名矩阵中的值
+     * @param hashFuncOrder 表示第多少个hash函数
+     * @param rowNumber 需要计算的行号值
+     * @return
+     */
+    private int handleHashFunction(int hashFuncOrder,int rowNumber){
+        switch (hashFuncOrder){
+            case 0:
+                return (rowNumber * 9 + 4) % News.itemSerialNumber;
+            case 1:
+                return (rowNumber * 6 + 67) % News.itemSerialNumber;
+            case 2:
+                return (rowNumber * 14 + 54) % News.itemSerialNumber;
+            case 3:
+                return (rowNumber * 32 + 167) % News.itemSerialNumber;
+        }
+        return Integer.MAX_VALUE;
     }
 
     /**
@@ -59,5 +90,26 @@ public class CalSimilarityTest {
     public void testArray(){
         int[][] arr = new int[3][3];
         System.out.println(arr[0][0]);
+    }
+
+    /**
+     * return之后不会再执行
+     */
+    @Test
+    public void testSwitch(){
+        int chooseValue = 1;
+        switch (chooseValue){
+            case 0:
+                System.out.println("0");
+                return ;
+            case 1:
+                System.out.println("1");
+                return ;
+            case 2:
+                System.out.println("2");
+                return ;
+        }
+        System.out.println("return");
+        return ;
     }
 }
